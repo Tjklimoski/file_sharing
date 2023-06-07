e212cca798241a63cfdc9ce8e4692630a3e2e7e9
TJ Klimoski <tjklimoski@gmail.com>
2023-06-06 21:21:30 -0400

    Setup multer for file upload on upload route

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import multer from "multer";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
+const upload = multer({ dest: "uploads" });
 
 app.set("view engine", "ejs");
 
@@ -11,7 +13,9 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/upload", (req, res) => {});
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.send("sup");
+});
 
 app.listen(port, (e) =>
   e ? console.error(e) : console.log(`app started on port ${port}`)
