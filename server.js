57d082c445922a5e313e843630b12ddfa1fdbd82
TJ Klimoski <tjklimoski@gmail.com>
2023-06-06 20:38:52 -0400

    Setup home route

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.set("view-engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("home page");
+});
+
 app.listen(port, (e) =>
   e ? console.error(e) : console.log(`app started on port ${port}`)
 );
