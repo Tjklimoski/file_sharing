c762e568a791772149d0ba81988b82f83bfc93b5
TJ Klimoski <tjklimoski@gmail.com>
2023-06-06 20:33:54 -0400

    Start server on port

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.listen(port, (e) =>
+  e ? console.error(e) : console.log(`app started on port ${port}`)
+);
