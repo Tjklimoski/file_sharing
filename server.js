6df29bb89fad529ddb89ffc0104c5631b4c68c63
TJ Klimoski <tjklimoski@gmail.com>
2023-06-07 13:19:10 -0400

    handle post request on file download route, and inncorect password

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const upload = multer({ dest: "uploads" });
 mongoose.connect(process.env.DB_URL);
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index", { fileLink: req.query?.fileLink });
@@ -25,6 +26,7 @@ app.post("/upload", upload.single("file"), async (req, res) => {
     originalName: req.file.originalname,
   };
 
+  // multer is letting us read req.body
   if (req.body.password != null && req.body.password !== "") {
     fileData.password = await bcrypt.hash(req.body.password, 10);
   }
@@ -36,12 +38,21 @@ app.post("/upload", upload.single("file"), async (req, res) => {
   res.redirect("/?" + query);
 });
 
-app.get("/file/:id", async (req, res) => {
+app.route("/file/:id").get(handleDownload).post(handleDownload);
+
+async function handleDownload(req, res) {
   const file = await File.findById(req.params.id);
 
   if (file.password != null) {
+    // here express.urlEncoded is letting us read req.body
     if (req.body.password == null) {
-      res.render("password");
+      res.render("password", { fileName: file.originalName });
+      return;
+    }
+
+    if (!(await bcrypt.compare(req.body.password, file.password))) {
+      res.render("password", { error: true });
+      //we return in order to skip the code that would download the file to the user.
       return;
     }
   }
@@ -49,7 +60,7 @@ app.get("/file/:id", async (req, res) => {
   await file.save();
 
   res.download(file.path, file.originalName);
-});
+}
 
 app.listen(port, (e) =>
   e ? console.error(e) : console.log(`app started on port ${port}`)
